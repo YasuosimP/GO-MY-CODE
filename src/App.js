eaae8473213efc40ab2b3e79ad8c20b5676b7db6
YasuosimP <hamza.ayachi43@gmail.com>
2021-10-03 09:31:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
+import Forme from './pages/forme'
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Forme/>;
 }
-
 export default App;
